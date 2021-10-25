3626f842d711c918279057877ff8bb3ceb7e2a59
Karldev7 <slslove7@gmail.com>
2021-10-25 22:25:43 +0900

    commit

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-const express = require('express');
-const app = express ();
+var express = require('express'); // 웹 서버 사용
+var fs = require('fs') // 파일 로드 사용
 
-app.listen(8080, function(){
-    console.log('listening on 8080')
+var app = express();
+var port = 3000;
 
+app.listen(port, function(){
+	console.log('Server Start, Port : ' + port);
 });
 
-app.get('/', function(req,res){
-    res.sendFile(__dirname + '/Iframe.html')
-})
-
-
+app.get('/', function(req, res){
+	fs.readFile('Iframe.html', function(error, data){
+		if(error){
+			console.log(error);
+		}else{
+			res.writeHead(200, {'Content-Type': 'text/html'});
+			res.end(data);
+		}
+	});
+});
